ce8b98f89280e242a70cf1358c60180c0292edcd
Jens Bender <jens.bender.001@gmail.com>
2024-03-27 15:15:56 +0100

    In create_mysql_database.sql, create the videos table

--- create_mysql_database.sql
+++ create_mysql_database.sql
@@ -2,9 +2,22 @@ CREATE DATABASE youtube_analytics;
 USE youtube_analytics;
 
 CREATE TABLE channels (
+    channel_id VARCHAR(255) PRIMARY KEY,
     channel_name VARCHAR(255),
+    views INT,
+    videos INT,
+    subscribers INT
+);
+
+CREATE TABLE videos (
+    video_id VARCHAR(255) PRIMARY KEY,
     channel_id VARCHAR(255),
-    view_count INT,
-    video_count INT,
-    subscriber_count INT
+    video_title VARCHAR(255),
+    video_description TEXT,
+    published_at VARCHAR(255),
+    video_duration VARCHAR(255),
+    views INT,
+    likes INT,
+    comments INT,
+    thumbnail_url VARCHAR(255)
 );
