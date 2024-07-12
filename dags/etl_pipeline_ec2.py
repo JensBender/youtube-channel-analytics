a57e4e0ceeb40cf3977719bfe4292ebbaa70a3bd
Jens Bender <jens.bender.001@gmail.com>
2024-07-12 09:23:16 +0200

    In etl_pipeline_ec2.py, change startdate

--- dags/etl_pipeline_ec2.py
+++ dags/etl_pipeline_ec2.py
@@ -1,6 +1,5 @@
 # Imports
 from airflow.decorators import dag, task
-from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 import os
 import pandas as pd
@@ -28,7 +27,7 @@ default_args = {
 @dag(
     dag_id="etl_pipeline",
     description="Extract data from YouTube API, transform, and load into MySQL database",
-    start_date=days_ago(0),
+    start_date=datetime(2024, 7, 12),
     schedule_interval="0 0 * * *",
     catchup=False,
     tags=["Jens", "data engineering"],
