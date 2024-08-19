4f8b58c7d5f1603bb2db5ae18b796b5b48773e1e
Jens Bender <jens.bender.001@gmail.com>
2024-08-19 12:09:10 +0200

    In huggingface_space subdirectory, in app.py, change twitter-roberta-base-sentiment to twitter-roberta-base-sentiment-latest

--- huggingface_space/app.py
+++ huggingface_space/app.py
@@ -2,22 +2,14 @@ import gradio as gr
 from transformers import pipeline
 
 # Initialize sentiment analysis pipelines
-roberta_pipeline = pipeline(task="sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")
+roberta_pipeline = pipeline(task="sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
 distilbert_pipeline = pipeline(task="sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-
-# Define mapping from model output labels to human-readable labels
-label_mapping = {
-    "LABEL_0": "Negative",
-    "LABEL_1": "Neutral",
-    "LABEL_2": "Positive"
-}
-
 
 # Function to perform sentiment analysis on a single YouTube comment
 def analyze_sentiment(text, model_choice):
     if model_choice == "RoBERTa":
         result = roberta_pipeline(text)[0]
-        label = label_mapping[result["label"]]  
+        label = result["label"]
         score = result["score"]
     else:
         result = distilbert_pipeline(text)[0]   
