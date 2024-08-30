010bc8f7bbccb2e2fa54c239e61190b62c20180f
Jens Bender <jens.bender.001@gmail.com>
2024-08-30 13:34:06 +0200

    In huggingface_space subdirectory, update app_roberta.py with the most recent code from app.py, but reduced to the roberta model

--- huggingface_space/app_roberta.py
+++ huggingface_space/app_roberta.py
@@ -1,24 +1,41 @@
 import gradio as gr
 from transformers import pipeline
+import pandas as pd
 
-# Initialize sentiment analysis pipeline using the RoBERTa model fine-tuned on Twitter data
-sentiment_pipeline = pipeline(task="sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
+# Initialize sentiment analysis pipeline
+roberta_pipeline = pipeline(
+    task="sentiment-analysis",
+    model="cardiffnlp/twitter-roberta-base-sentiment-latest",
+    truncation=True,
+    max_length=512
+)
 
-# Function to perform sentiment analysis on a single YouTube comment
-def analyze_sentiment(text):
-    result = sentiment_pipeline(text)[0]
-    label = result["label"]
-    score = result["score"]
-    return f"Sentiment: {label}\nConfidence: {score:.3f}"
-
+# Function to perform sentiment analysis on multiple YouTube comments
+def analyze_sentiment(data):
+    # Convert comments from a list in the JSON input data to a Pandas DataFrame
+    df = pd.DataFrame(data["comment_text"], columns=["comment_text"])
+    
+    # Perform sentiment analysis
+    df["result"] = df["comment_text"].apply(lambda x: roberta_pipeline(x)[0])
+   
+    # Extract sentiment and confidence into separate columns
+    df["roberta_sentiment"] = df["result"].apply(lambda x: x["label"].lower())
+    df["roberta_confidence"] = df["result"].apply(lambda x: round(x["score"], 3))
+    
+    # Output data in JSON format
+    output = {
+        "roberta_sentiment": df["roberta_sentiment"].tolist(),
+        "roberta_confidence": df["roberta_confidence"].tolist()
+    }    
+    return output
 
 # Create Gradio interface
 iface = gr.Interface(
     fn=analyze_sentiment,
-    inputs=gr.Textbox(lines=3, placeholder="Enter text here..."),
-    outputs="text",
+    inputs=gr.JSON(),
+    outputs=gr.JSON(),
     title="Sentiment Analysis with RoBERTa",
-    description="Enter a text to analyze its sentiment."
+    description="Send a JSON object with a list of comments via API request for sentiment analysis."
 )
 
 # Launch Gradio interface as a web application
